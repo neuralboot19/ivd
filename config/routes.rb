2d6178883e0dd18dfc799f9a147705d8e74bc1cc
Jose Castellanos <neuralboot19@gmail.com>
2023-01-20 11:46:35 +0100

    Feacture Login with protect pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :authentication, path: '', as: '' do
+    resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
+  end
+
   root "dashboard#index"
 end
